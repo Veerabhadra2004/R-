1fc597afefa014ff6e0e1c15696a96b8973825e7
Veerabhadra2004 <veerag.23.beai@acharya.ac.in>
2024-09-17 16:15:31 +0530

    BASIC

---
+++ BASIC.R
@@ -0,0 +1,27 @@
+#this is a program
+print("veerabhadra")
+print("1AY23AIO39")
+print(8.3)
+
+#add two numbers
+a=10
+b=30
+c=a+b
+
+help("reserved")
+
+a=10
+class(a)
+typeof(a)
+
+c="hello"
+class(c)
+typeof(c)
+d='hello'
+class(d)
+typeof(d)
+
+e='h'
+f="r"
+class(e)
+typeof(e)
